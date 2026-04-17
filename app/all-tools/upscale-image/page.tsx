77fea1e1447c0f49fd9eb1dd9f34525898dbb984
simplifymove <raghavaboyidi@gmail.com>
2026-04-17 10:35:19 -0400

    fix: Remove duplicate closing tag in upscale-image component

--- app/all-tools/upscale-image/page.tsx
+++ app/all-tools/upscale-image/page.tsx
@@ -301,7 +301,6 @@ export default function UpscaleImagePage() {
                           setImageLoading(false);
                         }}
                       />
-                      />
                     </div>
 
                     {/* Metadata Display */}
